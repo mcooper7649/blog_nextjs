f63e038b324bae637837fccbf0897cb180b18eb6
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-03-24 14:06:35 -0400

    added env variable toggle and ran build and optimized:

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,19 +1,17 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import {
-  atomDark,
-  shadesOfPurple,
-  coldarkCold,
-  solarizedDarkAtom,
-  materialDark,
-} from 'react-syntax-highlighter/dist/cjs/styles/prism';
-
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
 function PostContent(props) {
   const { post } = props;
+
+  SyntaxHighlighter.registerLanguage('js', js);
+  SyntaxHighlighter.registerLanguage('css', css);
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
