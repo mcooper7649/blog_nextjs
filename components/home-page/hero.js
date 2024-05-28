457f2f9bb55aed48cae8b2130bbb978ed5d3d9bb
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-05-28 13:53:07 -0400

    Remove hero animations and updat gitignore

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,31 +1,18 @@
-import Image from 'next/image';
-import { AnimatePresence, motion } from 'framer-motion/dist/framer-motion';
+import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion/dist/framer-motion";
 
-import classes from './hero.module.css';
+import classes from "./hero.module.css";
 
 function Hero() {
   return (
     <section className={classes.hero}>
-      <motion.div
-        className={classes.image}
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-
-          borderRadius: ['20%', '20%', '50%', '50%', '20%'],
-        }}
-      >
-        <Image
-          src="/images/site/myimage.png"
-          alt="An image showing Mike"
-          width={300}
-          height={300}
-        />
-      </motion.div>
-
-      <motion.h1 initial={{ x: 100 }} animate={{ x: 10 }} className="hero">
-        Find your inner ninja 🥷
-      </motion.h1>
+      <Image
+        src="/images/site/myimage.png"
+        alt="An image showing Mike"
+        width={300}
+        height={300}
+      />
+      Find your inner ninja 🥷
     </section>
   );
 }
